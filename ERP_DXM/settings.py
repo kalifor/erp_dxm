07f4e3239e4fab6485c81204126bf954dd6925af
DIOGO.XIMENES <DIOGOXM@GMAIL.COM>
2017-06-21 15:24:49 -0300

    Ajustes para deploy 3.

--- ERP_DXM/settings.py
+++ ERP_DXM/settings.py
@@ -98,8 +98,7 @@ ROOT_URLCONF = 'ERP_DXM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
